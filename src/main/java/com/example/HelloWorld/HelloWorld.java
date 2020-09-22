e119c4ace8213773d05a715abce26f561735c368
Raghu <raghureddyknl@gmail.com>
2020-09-22 09:21:44 +0530

    first commit

---
+++ src/main/java/com/example/HelloWorld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example.HelloWorld;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+	@RequestMapping("/")
+	String home() {
+		return "Hello World";
+	}
+}
